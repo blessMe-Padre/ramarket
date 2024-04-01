1ea32e339180131341aa9a3225d893d38b0d6052
Сергей Субботин <zarodiny@yandex.ru>
2024-04-02 09:42:52 +1000

    feat: добавляет стили

--- searchform.php
+++ searchform.php
@@ -1,7 +1,7 @@
 <form class="header__search" role="search" method="get" id="searchform" action="<?php echo home_url('/') ?>">
     <label class="screen-reader-text" hidden for="s">Поиск: </label>
 
-    <input type="text" value="<?php echo get_search_query() ?>" name="s" id="s" placeholder="поиск по товарам" />
+    <input type="text" value="<?php echo get_search_query() ?>" name="s" id="s" placeholder="Поиск по товарам..." />
 
     <button type="submit" id="searchsubmit" value="найти">
         <img src="<?php echo get_template_directory_uri(); ?>/src/img/icons/icon-search.svg" width="21" height="21"

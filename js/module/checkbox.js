08a28ef48b0f761c21714473337cc5f008cbe737
Сергей Субботин <zarodiny@yandex.ru>
2024-04-02 14:02:18 +1000

    feat: добавляет стили

--- js/module/checkbox.js
+++ js/module/checkbox.js
@@ -24,12 +24,41 @@ export const initCheckBox = () => {
         }
     });
 
-    const umField = document.querySelector('#um_field_93_avatar');
-    if (umField) {
-        const umClears = umField.querySelectorAll('.um-clear');
-        umClears.forEach((item) => {
-            console.log(item);
-            item.remove();
-        })
+
+    // ==================================================================================================
+
+    const checkboxes1 = document.querySelectorAll('.filter-main .wpfLiLabel .wpfCheckbox input[type="checkbox"]');
+    // Функция для обновления класса 'is-active'
+    function updateActiveClass() {
+        // Проверяем, установлен ли флажок
+        if (this.checked) {
+            // Если да, добавляем класс 'is-active' к родительскому элементу 'wpfLiLabel'
+            this.closest('.wpfCheckbox').classList.add('is-active');
+        } else {
+            // Если нет, удаляем класс 'is-active'
+            this.closest('.wpfCheckbox').classList.remove('is-active');
+        }
     }
+
+    // Добавляем обработчик событий к каждому чекбоксу
+    checkboxes1.forEach(function (checkbox) {
+        checkbox.addEventListener('change', updateActiveClass);
+
+        // Начальное обновление классов при загрузке страницы
+        if (checkbox.checked) {
+            checkbox.closest('.wpfCheckbox').classList.add('is-active');
+        }
+    });
+
+
+
+
+    // const umField = document.querySelector('#um_field_93_avatar');
+    // if (umField) {
+    //     const umClears = umField.querySelectorAll('.um-clear');
+    //     umClears.forEach((item) => {
+    //         console.log(item);
+    //         item.remove();
+    //     })
+    // }
 }
